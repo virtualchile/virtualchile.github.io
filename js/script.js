f93750e86e808cf9bba7d921e0cd39623ced4cfe
alelepd <PLEIQ@MacBook-Pro-de-PLEIQ.local>
2016-04-05 17:12:16 -0300

    update stuff

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(function($) {
+
+  //This opens the contact form modal
+  	$(".contact-button").on("click", function(){
+  		$(".contact-us-container").fadeIn();
+  	});
+
+  	$(".contact-us-shadow").on("click", function(){
+  		$(".contact-us-container").fadeOut();
+  	});
+
+  	$(".close-btn").on("click", function(){
+  		$(".contact-us-container").fadeOut();
+  	});
+
+    $(".menu-icon").on("click", function(){
+  		$(".navigation-container").fadeToggle();
+      $(".menu-icon").toggleClass("close-mode");
+
+  	});
+
+    $(".map-hover").on("click", function(){
+  		// $(".navigation-container").fadeToggle();
+      $(".map-hover").toggleClass("map-hover-unlock");
+
+  	});
+
+
+  // This manages the navigation bar so it becomes prominent on scroll
+  $(window).bind('scroll', function() {
+  var navHeight = 1;
+    if ($(window).scrollTop() > navHeight) {
+      $('.site-navigation').addClass('site-navigation-fixed');
+    }
+    else {
+      $('.site-navigation').removeClass('site-navigation-fixed');
+    }
+  });
+
+  // This will make an autoscroll to the bottom of the page (CTA)
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+
+
+
+});
--- playground/about/index.php
+++
@@ -1,5 +0,0 @@
-
-<?php  include "../head.php"?>
-<?php  include "../content-about.php"?>
-<?php  include "../pre-footer.php"?>
-<?php  include "../footer.php"?>
