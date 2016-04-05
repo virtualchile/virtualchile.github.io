ac126ebee27df5bf6b46cdb6299f08cf860ba3c5
alelepd <PLEIQ@MacBook-Pro-de-PLEIQ.local>
2016-04-05 01:15:59 -0300

    update scenejs

--- playground/js/scene.js
+++ playground/js/scene.js
@@ -20,8 +20,8 @@ function onWindowResize() {
 
 
 if (window.innerWidth <= 770) {
-	renderer.setSize( window.innerWidth, 440);
-	camera.aspect = window.innerWidth / 440;
+	renderer.setSize( window.innerWidth, 500);
+	camera.aspect = window.innerWidth / 500;
 } else {
 	renderer.setSize( window.innerWidth, rendererHeight);
 	camera.aspect = window.innerWidth / rendererHeight;
